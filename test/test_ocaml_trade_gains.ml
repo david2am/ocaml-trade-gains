050c0820b7028bd7f47e2ba4ff287b706275df48
David <David.De.Vargas.Cueter.-ND@disney.com>
2025-04-30 15:45:34 -0500

    Test test case

--- test/test_ocaml_trade_gains.ml
+++ test/test_ocaml_trade_gains.ml
@@ -1,9 +1,13 @@
-let test_hello () =
-  Alcotest.(check string) "same string" "hello" "hello"
+(* Tests Cases *)
+let test_float_list () =
+  let expect = [None; None; Some 1.] in
+  let actual = [None; None; Some 1.] in
+  
+  Alcotest.(check @@ list @@ option @@ float 0.0001) "Case 1" expect actual
 
 let () =
-  Alcotest.run "My Project" [
-    "hello", [
-      Alcotest.test_case "Hello test" `Quick test_hello;
-    ];
+  Alcotest.run "Trade Taxes" [
+    "list validation", [
+      Alcotest.test_case "validate float list" `Quick test_float_list;
+    ]
   ]
