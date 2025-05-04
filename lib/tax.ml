e08ee1e6577d635d42fbefdcf6f16009872c9073
David <David.De.Vargas.Cueter.-ND@disney.com>
2025-05-04 18:15:49 -0500

    Refact/parameterizing test_data

--- lib/tax.ml
+++ lib/tax.ml
@@ -2,61 +2,66 @@ type data = { unit_cost : float ; quantity : int }
 type trade = Buy of data | Sell of data
 type trade_result = Loss | Draw | Win
 
-let calculate_amount data = data.unit_cost *. Int.to_float data.quantity
+let calculate_amount data = 
+  data.unit_cost *. Int.to_float data.quantity
 
-let calculate_weight_avg prev_weight_avg stock_qty data =
-  let { unit_cost; quantity } = data in
+let calculate_weight_avg old_weight_avg stocks { unit_cost; quantity } =
   let quantity = Int.to_float quantity in
-  let prev_quantity = Int.to_float stock_qty in
+  let stocks = Int.to_float stocks in
 
-  if prev_quantity = 0. then unit_cost
-  else (prev_quantity *. prev_weight_avg +. quantity *. unit_cost) /. (prev_quantity +. quantity)
+  if stocks = 0. then
+    unit_cost
+  else
+    (stocks *. old_weight_avg +. quantity *. unit_cost) /. (stocks +. quantity)
 
-let calculate_profit weight_avg data = (Int.to_float data.quantity) *. (data.unit_cost -. weight_avg)
+let calculate_profit weight_avg data = 
+  (Int.to_float data.quantity) *. (data.unit_cost -. weight_avg)
 
-(* Sell *)
-let map_gross_profit profit =
+let operation_result profit =
   match compare profit 0. with
     | -1 -> Loss
     |  1 -> Win
     |  _ -> Draw
 
-let calculate_tax profit tax_percent = profit *. tax_percent
+let calculate_tax profit tax_percent =
+  profit *. tax_percent
 
-let calculate_net_profit loss profit =
+let calculate_loss__net_profit loss profit =
   if loss > profit then
-    (loss -. profit, 0.)  (* profits are deducted from losses, no profit *)
+    loss -. profit, 0.  (* profits are deducted from losses, no profit *)
   else
-    (0., profit -. loss)  (* no loss, losses are deducted from profits *)
+    0., profit -. loss  (* no loss, losses are deducted from profits *)
 
 let calc_tax_builder threshold tax_percent =
   let weight_avg = ref 0. in
   let stock_qty = ref 0 in
-  let loss = ref 0. in
+  let acc_loss = ref 0. in
 
   fun trade ->
     match trade with
     | Buy data ->
       weight_avg := calculate_weight_avg !weight_avg !stock_qty data;
-      stock_qty := !stock_qty + data.quantity;
-      None
+      stock_qty := !stock_qty + data.quantity; None
     | Sell data ->
       if !stock_qty < data.quantity then
         failwith "Cannot sell more stocks than owned";
 
       let gross_profit = calculate_profit !weight_avg data in
       stock_qty := !stock_qty - data.quantity;
-      begin match map_gross_profit gross_profit with
+
+      begin match operation_result gross_profit with
       | Loss ->
-        loss := !loss -. gross_profit; None
+        acc_loss := !acc_loss -. gross_profit; None
       | Draw -> None
       | Win -> 
-        let loss_, net_profit = calculate_net_profit !loss gross_profit in
-        loss := loss_;
+        let loss, net_profit = calculate_loss__net_profit !acc_loss gross_profit in
+        acc_loss := loss;
         let amount = calculate_amount data in
-        let is_taxable = (gross_profit > threshold || amount > threshold) && net_profit > 0. in
 
-        if is_taxable then Some (calculate_tax net_profit tax_percent ) else None
+        if amount > threshold && net_profit > 0. then
+          Some (calculate_tax net_profit tax_percent)
+        else
+          None
       end
 
 let print_tax t =
@@ -64,4 +69,5 @@ let print_tax t =
   | None -> print_endline "No Tax"
   | Some amount -> Printf.printf "Tax %.2f\n" amount
 
-let print_tax_list taxes = List.iter print_tax taxes
+let print_tax_list taxes = 
+  List.iter print_tax taxes
--- test/test_ocaml_trade_gains.ml
+++ test/test_ocaml_trade_gains.ml
@@ -1,138 +1,120 @@
 open Trade.Tax
 
-(* Case 1 *)
-let data_1 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 100 };
-    Sell { unit_cost = 15.; quantity = 50  };
-    Sell { unit_cost = 15.; quantity = 50  };
-  ],
-  [None; None; None]
-)
+(* Test cases data *)
+let test_data = [
+  (* Case 1 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 100 };
+      Sell { unit_cost = 15.; quantity = 50  };
+      Sell { unit_cost = 15.; quantity = 50  };
+    ],
+    [None; None; None]
+  );
+  (* Case 2 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Sell { unit_cost = 20.; quantity = 5000  };
+      Sell { unit_cost = 5.;  quantity = 5000  };
+    ],
+    [None; Some 10000.; None]
+  );
+  (* Case 3 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Sell { unit_cost = 5.;  quantity = 5000  };
+      Sell { unit_cost = 20.; quantity = 3000  };
+    ],
+    [None; None; Some 1000.]
+  );
+  (* Case 4 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Buy  { unit_cost = 25.; quantity = 5000  };
+      Sell { unit_cost = 15.; quantity = 10000 };
+    ],
+    [None; None; None]
+  );
+  (* Case 5 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Buy  { unit_cost = 25.; quantity = 5000  };
+      Sell { unit_cost = 15.; quantity = 10000 };
+      Sell { unit_cost = 25.; quantity = 5000  };
+    ],
+    [None; None; None; Some 10000.]
+  );
+  (* Case 6 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Sell { unit_cost = 2.;  quantity = 5000  };
+      Sell { unit_cost = 20.; quantity = 2000  };
+      Sell { unit_cost = 20.; quantity = 2000  };
+      Sell { unit_cost = 25.; quantity = 1000  };
+    ],
+    [None; None; None; None; Some 3000.]
+  );
+  (* Case 7 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Sell { unit_cost = 2.;  quantity = 5000  };
+      Sell { unit_cost = 20.; quantity = 2000  };
+      Sell { unit_cost = 20.; quantity = 2000  };
+      Sell { unit_cost = 25.; quantity = 1000  };
+      Buy  { unit_cost = 20.; quantity = 10000 };
+      Sell { unit_cost = 15.; quantity = 5000  };
+      Sell { unit_cost = 30.; quantity = 4350  };
+      Sell { unit_cost = 30.; quantity = 650   };
+    ],
+    [None; None; None; None; Some 3000.; None; None; Some 3700.; None]
+  );
+  (* Case 8 *)
+  (
+    [
+      Buy  { unit_cost = 10.; quantity = 10000 };
+      Sell { unit_cost = 50.; quantity = 10000 };
+      Buy  { unit_cost = 20.; quantity = 10000 };
+      Sell { unit_cost = 50.; quantity = 10000 };
+    ],
+    [None; Some 80000.;None; Some 60000.]
+  );
+  (* Case 9 *)
+  (
+    [
+      Buy  { unit_cost = 5000.;  quantity = 10 };
+      Sell { unit_cost = 4000.;  quantity = 5  };
+      Buy  { unit_cost = 15000.; quantity = 5  };
+      Buy  { unit_cost = 4000.;  quantity = 2  };
+      Buy  { unit_cost = 23000.; quantity = 2  };
+      Sell { unit_cost = 20000.; quantity = 1  };
+      Sell { unit_cost = 12000.; quantity = 10 };
+      Sell { unit_cost = 15000.; quantity = 3  };
+    ],
+    [None; None; None; None; None; None; Some 1000.; Some 2400.]
+  );
+]
 
+(* Create test cases from the data *)
+let create_test_case idx (input, expect) =
+  let test_name = Printf.sprintf "Case %d" (idx + 1) in
+  let test_fun () =
+    let threshold = 20000. in
+    let tax_percent = 0.2 in
+    let calc_tax = calc_tax_builder threshold tax_percent in
 
-(* Case 2 *)
-let data_2 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 10000 };
-    Sell { unit_cost = 20.; quantity = 5000  };
-    Sell { unit_cost = 5.;  quantity = 5000  };
-  ],
-  [None; Some 10000.; None]
-)
-
-
-(* Case 3 *)
-let data_3 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 10000 };
-    Sell { unit_cost = 5.;  quantity = 5000  };
-    Sell { unit_cost = 20.; quantity = 3000  };
-  ],
-  [None; None; Some 1000.]
-)
-
-
-(* Case 4 *)
-let data_4 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 10000 };
-    Buy  { unit_cost = 25.; quantity = 5000  };
-    Sell { unit_cost = 15.; quantity = 10000 };
-  ],
-  [None; None; None]
-)
-
-
-(* Case 5 x *)
-let data_5 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 10000 };
-    Buy  { unit_cost = 25.; quantity = 5000  };
-    Sell { unit_cost = 15.; quantity = 10000 };
-    Sell { unit_cost = 25.; quantity = 5000  };
-  ],
-  [None; None; None; Some 10000.]
-)
-
-
-(* Case 6 *)
-let data_6 = [
-  Buy  { unit_cost = 10.; quantity = 10000 };
-  Sell { unit_cost = 2.;  quantity = 5000  };
-  Sell { unit_cost = 20.; quantity = 2000  };
-  Sell { unit_cost = 20.; quantity = 2000  };
-  Sell { unit_cost = 25.; quantity = 1000  };
-],
-[None; None; None; None; Some 3000.]
-
-
-(* Case 7 x *)
-let data_7 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 10000 };
-    Sell { unit_cost = 2.;  quantity = 5000  };
-    Sell { unit_cost = 20.; quantity = 2000  };
-    Sell { unit_cost = 20.; quantity = 2000  };
-    Sell { unit_cost = 25.; quantity = 1000  };
-    Buy  { unit_cost = 20.; quantity = 10000 };
-    Sell { unit_cost = 15.; quantity = 5000  };
-    Sell { unit_cost = 30.; quantity = 4350  };
-    Sell { unit_cost = 30.; quantity = 650   };
-  ],
-  [None; None; None; None; Some 3000.; None; None; Some 3700.; None]
-)
-
-
-(* Case 8 x *)
-let data_8 = (
-  [
-    Buy  { unit_cost = 10.; quantity = 10000 };
-    Sell { unit_cost = 50.; quantity = 10000 };
-    Buy  { unit_cost = 20.; quantity = 10000 };
-    Sell { unit_cost = 50.; quantity = 10000 };
-  ],
-  [None; Some 80000.;None; Some 60000.]
-)
-
-
-(* Case 9 x *)
-let data_9 = (
-  [
-    Buy  { unit_cost = 5000.;  quantity = 10 };
-    Sell { unit_cost = 4000.;  quantity = 5  };
-    Buy  { unit_cost = 15000.; quantity = 5  };
-    Buy  { unit_cost = 4000.;  quantity = 2  };
-    Buy  { unit_cost = 23000.; quantity = 2  };
-    Sell { unit_cost = 20000.; quantity = 1  };
-    Sell { unit_cost = 12000.; quantity = 10 }; (* x 2000 *)
-    Sell { unit_cost = 15000.; quantity = 3  };
-  ],
-  [None; None; None; None; None; None; Some 1000.; Some 2400.]
-)
-
-let test_case (input, expect) = fun () ->
-  let threshold = 20000. in
-  let tax_percent = 0.2 in
-  let calc_tax = calc_tax_builder threshold tax_percent in
-
-  let expect = expect in
-  let actual = List.map calc_tax input in
-  
-  Alcotest.(check (float 0.0001 |> option |> list)) "Case 1" expect actual
-
+    let actual = List.map calc_tax input in
+    
+    Alcotest.(check (float 0.0001 |> option |> list)) test_name expect actual
+  in
+  Alcotest.test_case test_name `Quick test_fun
 
 let () =
   Alcotest.run "Trade Taxes" [
-    "list validation", [
-      Alcotest.test_case "Case 1" `Quick @@ test_case data_1;
-      Alcotest.test_case "Case 2" `Quick @@ test_case data_2;
-      Alcotest.test_case "Case 3" `Quick @@ test_case data_3;
-      Alcotest.test_case "Case 4" `Quick @@ test_case data_4;
-      Alcotest.test_case "Case 5" `Quick @@ test_case data_5;
-      Alcotest.test_case "Case 6" `Quick @@ test_case data_6;
-      Alcotest.test_case "Case 7" `Quick @@ test_case data_7;
-      Alcotest.test_case "Case 8" `Quick @@ test_case data_8;
-      Alcotest.test_case "Case 9" `Quick @@ test_case data_9;
-    ]
+    "Tax Calculation Tests", List.mapi create_test_case test_data 
   ]
